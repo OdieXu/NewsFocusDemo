7a824cea99ccd2be386d9f808f05bf8b00d73d4c
OdieXu <xurui19875050@163.com>
2015-04-14 10:23:56 +0800

    Initial Commit

---
+++ NewsFocusDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsFocusDemo
+//
+//  Created by 徐锐 on 15/4/14.
+//  Copyright (c) 2015年 徐锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
